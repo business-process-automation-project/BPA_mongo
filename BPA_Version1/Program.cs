5aec3976474b7172ffa234c13b9d19cc5bdf0c95
Luisa <D064979@DRSN34399114A>
2019-05-10 15:55:05 +0200

    questions and changes on programm

--- BPA_Version1/Program.cs
+++ BPA_Version1/Program.cs
@@ -9,39 +9,37 @@ using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Driver;
 
+
 namespace BPA_Version1
 {
     class Program
     {
         public static MongoClient client = new MongoClient("mongodb://localhost:27017");
+        public static IMongoDatabase  database = client.GetDatabase("BPA");
+        public static IMongoCollection<BsonDocument> Qcollection = database.GetCollection<BsonDocument>("Question");
 
         static void Main(string[] args)
         {
-            //Verbindung aufbauen 
-            var database = client.GetDatabase("BPA");
-            
-            //Json einlesen 
-            string text = System.IO.File.ReadAllText(@"C:\Users\D064979\Desktop\json\json.json");
-            var document = BsonSerializer.Deserialize<BsonDocument>(text);
-            var collection = database.GetCollection<BsonDocument>("Fragen");
-            collection.InsertOne(document);
-
-            //mehrere Strings einlesen Test
-            string myjson = System.IO.File.ReadAllText(@"C:\Users\D064979\Desktop\json\json2.json");
-            var doc = new BsonDocument {
-                    { "values", BsonSerializer.Deserialize<BsonArray>(myjson) }
-                };
-            collection.InsertOne(doc); 
-
-
-            //String einlesen mit Methode 
-            string path = "C:\\Users\\D064979\\Desktop\\json\\json1.json";
-            ReadJson("BPA", path, "Fragen"); 
-            ShowDB();
-            DeleteDB("BPA");
+            Program p = new Program();
+            string path = "C:\\Users\\D064979\\Desktop\\json\\json1.json"; 
+            p.SaveQuestion(path);
+            p.DeleteOneQuestion("true", 2);
+            p.ShowDB();
+            p.DeleteCollection("Question"); 
+                       
         }
 
-        static void ShowDB() {
+        public void DeleteOneQuestion( string attribute, int value)
+        {
+            Qcollection.DeleteOne(new BsonDocument(attribute, value)); 
+        }
+
+        public void DeleteCollection(string name)
+        {
+            database.DropCollection(name); 
+        }
+
+        public void ShowDB() {
             var dbList = client.ListDatabases().ToList();
             Console.WriteLine("The list of databases are :");
             foreach (var item in dbList)
@@ -56,15 +54,41 @@ namespace BPA_Version1
             client.DropDatabase(name); 
         }
 
-        static void ReadJson(string nameDb, string file, string collecName)
+        public void SaveQuestion ( string file)
         {
-            var database = client.GetDatabase(nameDb);
+            
             string text = System.IO.File.ReadAllText(@file);
             var document = BsonSerializer.Deserialize<BsonDocument>(text);
-            var collection = database.GetCollection<BsonDocument>(collecName);
-            collection.InsertOne(document);
+            Qcollection.InsertOne(document);
         }
 
+
+        public void oldCode()
+        {
+            //Json einlesen 
+            string text = System.IO.File.ReadAllText(@"C:\Users\D064979\Desktop\json\json.json");
+            var document = BsonSerializer.Deserialize<BsonDocument>(text);
+            var database = client.GetDatabase("Test");
+            var collection = database.GetCollection<BsonDocument>("Fragen");
+            collection.InsertOne(document);
+
+            //mehrere Strings einlesen Test
+            string myjson = System.IO.File.ReadAllText(@"C:\Users\D064979\Desktop\json\json2.json");
+            var doc = new BsonDocument {
+                    { "values", BsonSerializer.Deserialize<BsonArray>(myjson) }
+                };
+            collection.InsertOne(doc);
+                                 
+
+            //Test: Document finden bei dem richtige Frage = 2 ist
+            var results = collection.CountDocuments(new BsonDocument("true", 2));
+            Console.WriteLine("{0} ist die Anzahl", results);
+
+
+            ShowDB();
+            DeleteDB("BPA");
+
+        }
       
     }
     

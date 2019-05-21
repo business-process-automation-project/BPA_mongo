cb8a1ed1d06c57f773749cb0d85e5199447f14af
Lennert <Lennert.berkhan@gmail.com>
2019-05-21 09:40:01 +0200

    added MongoInit, added Comments to Methods

--- BPA_Version1/Program.cs
+++ BPA_Version1/Program.cs
@@ -17,73 +17,127 @@ namespace BPA_Version1
 {
     class Program
     {
+        //MongoDB Globale Settings
         public static MongoClient client = new MongoClient("mongodb://localhost:27017");
         public static IMongoDatabase  database = client.GetDatabase("BPA");
         public static IMongoCollection<BsonDocument> Qcollection = database.GetCollection<BsonDocument>("Question");
-        public static MqttClient mqttclient = new MqttClient(IPAddress.Parse("141.56.180.120"));
-        public static string channel = "BPA";
-        public static Program p = new Program();
+        public static IMongoCollection<BsonDocument> Pcollection = database.GetCollection<BsonDocument>("Player");
+        
+        //MQTT Globale Settings mit Brocker und Topics
+        public static MqttClient mqtt = new MqttClient(IPAddress.Parse("141.56.180.120"));
+        public static String[] topics = {"BPA", "TEST","REGISTER"};
 
+        public static Program p = new Program();
+        
         static void Main(string[] args)
         {
+            p.SetMQTT(); //MQTT Connection aufbauen
+            p.MongoInit(); //MongoDB mit Fragen initialisieren
+        }
 
-            mqttclient.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
-            var clientId = Guid.NewGuid().ToString();
-            mqttclient.Connect(clientId);
-            mqttclient.Subscribe(
-                new string[] {channel},
-                new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
-                      
+        //MQTT Client verbinden und Topics subscriben
+        public bool SetMQTT(){
+            mqtt.MqttMsgPublishReceived += MQTTHandler;        
+            mqtt.Connect("Spielmanager","","",false,ushort.MaxValue);
 
-           
-            //zum Testen 
-            p.DeleteCollection("Question");
-        
-            //Initiales Laden der Fragen aus dem Ordner
-            System.IO.DirectoryInfo ParentDirectory = new System.IO.DirectoryInfo(@"C:\json");
-            foreach (System.IO.FileInfo f in ParentDirectory.GetFiles())
+            if(mqtt.IsConnected)
             {
-                p.SaveQuestion(f.FullName.ToString());
+                Console.WriteLine("MQTT connected.");
+                byte[] qos = new byte[topics.Length];
+                for(int i=0; i < topics.Length;i++)
+                {
+                    qos[i] = MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE;
+                }
+
+                mqtt.Subscribe(topics, qos);
+                return true;
+            } else 
+            {
+                Console.WriteLine("MQTT NOT connected.");
+                return false;
             }
-
-            
-
-
-
-
-
         }
 
-        public void MQTTPublish(string msg)
+        //MQTT Senden von Nachrichten
+        public void MQTTPublish(string channel, string msg)
         {
-            mqttclient.Publish(channel, Encoding.UTF8.GetBytes(msg));
-
+            mqtt.Publish(channel, Encoding.UTF8.GetBytes(msg));
         }
 
-
-        static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        //MQTT Verarbeiten von eingegangen Nachrichten auf verschiedenen Topics
+        static void MQTTHandler(object sender, MqttMsgPublishEventArgs e)
         {
-            // handle message received, get message as bitarray e -> msg string
             string msg = Encoding.UTF8.GetString(e.Message, 0, e.Message.Length);
-
-            Console.WriteLine("message = " + msg);
-            if (msg == "Fragen")
+            switch (e.Topic)
             {
-               p.MQTTPublish(p.SelectQuestion("true", "2"));
-
+                case "BPA":
+                    Console.WriteLine("I'm a message from topic BPA\t{0}",msg);
+                    break;
+                case "TEST":
+                    Console.WriteLine("I'm a message from topic TEST\t{0}",msg);
+                    break;
+                case "Luisa":
+                    if (msg == "Fragen")
+                    {
+                        p.MQTTPublish("Luisa",p.SelectQuestion("true", "2"));
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Topic {0} is not defined. \tMessage = {1}",e.Topic, msg);
+                    break;
             }
         }
 
+        //MongoDB Initiales laden der Fragen in die MongoDB
+        public bool MongoInit()
+        {
+            try
+            {
+                p.DeleteCollection("Question");
+                System.IO.DirectoryInfo ParentDirectory = new System.IO.DirectoryInfo(@"C:\json");
+                foreach (System.IO.FileInfo f in ParentDirectory.GetFiles())
+                {
+                    p.SaveQuestion(f.FullName.ToString());
+                }
+                
+                long count = Qcollection.Count(new BsonDocument());
+
+                if(count>0)
+                {
+                    Console.WriteLine("Added {0} questions to the database.",count);
+                    return true;
+                } else
+                {
+                    Console.WriteLine("No questions added to the database.",count);
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("MongoDB initialization failed. \nException: {0}",e.Message);
+                return false;
+            }
+        }
+ 
+        //MongoDB Löschen einer Frage
         public void DeleteOneQuestion( string attribute, int value)
         {
             Qcollection.DeleteOne(new BsonDocument(attribute, value)); 
         }
 
+        //MongoDB Löschen einer Collection
         public void DeleteCollection(string name)
         {
             database.DropCollection(name); 
         }
 
+        //MongoDB Löschen einer Datenbank
+        static void DeleteDB(string name)
+        {
+            client.DropDatabase(name); 
+        }
+
+        //MongoDB Ausgabe aller Datenbanken auf der MongoDB
         public void ShowDB() {
             var dbList = client.ListDatabases().ToList();
             Console.WriteLine("The list of databases are :");
@@ -94,19 +148,15 @@ namespace BPA_Version1
             Console.ReadKey();
         }
 
-        static void DeleteDB(string name)
-        {
-            client.DropDatabase(name); 
-        }
-
+        //MongoDB Speichern einer Frage
         public void SaveQuestion (string file)
         {
-            
             string text = System.IO.File.ReadAllText(@file);
             var document = BsonSerializer.Deserialize<BsonDocument>(text);
             Qcollection.InsertOne(document);
         }
         
+        //MongoDB Speichern mehrerer Fragen
         public void SaveMultipleQuestions(string name)
         {
             string myjson = System.IO.File.ReadAllText(@name);
@@ -116,7 +166,7 @@ namespace BPA_Version1
             Qcollection.InsertOne(doc);
         }
 
-
+        //MongoDB Select einer Frage
         public string SelectQuestion (string att, string numb)
         {
             var filter = Builders<BsonDocument>.Filter.Eq(att, numb);
@@ -124,14 +174,8 @@ namespace BPA_Version1
             foreach (var doc in result)
             {
                 Console.WriteLine(doc.ToJson());
-
             }
             return result.ToString(); 
-        }
-
-
-       
-      
-    }
-    
+        }      
+    }    
 }
